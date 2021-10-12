4898c89346adf274d2d6ab9dad20d56b1488ac2c
Alpha17-2 <alphaisgod1@gmail.com>
2021-10-12 22:47:17 +0530

    Implementing UI

--- lib/Screens/authScreen.dart
+++ lib/Screens/authScreen.dart
@@ -23,25 +23,51 @@ class authScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Image.asset(
-                'images/friends.png',
-                height: displayHeight(context) * 0.3,
-                width: displayWidth(context) * 0.6,
-                fit: BoxFit.cover,
-              ),
               Card(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(15),
                 ),
                 elevation: 12,
                 child: Container(
-                  height: displayHeight(context) * 0.6,
+                  height: displayHeight(context) * 0.8,
                   width: displayWidth(context) * 0.9,
                   decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(15)),
-                  child: Column(
-                    children: [],
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top:16.0),
+                          child: Text("Meet my friends",style: TextStyle(
+                            color: Colors.teal,
+                            fontSize: displayWidth(context)*0.08,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Kaushan',
+                          ),),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top:10.0,left: 4,right: 4),
+                          child: Text('Distance means so little, when someone means so much .',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: displayWidth(context)*0.04,
+
+                          ),),
+                        ),
+                        Image.asset(
+                          'images/friends.png',
+                          height: displayHeight(context) * 0.25,
+                          width: displayWidth(context) * 0.4,
+                          fit: BoxFit.cover,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               )
