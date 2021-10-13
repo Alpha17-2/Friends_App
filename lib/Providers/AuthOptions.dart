da0e6a12ba4e3388817e42cb676de5c52a9f7c13
Alpha17-2 <alphaisgod1@gmail.com>
2021-10-13 08:42:54 +0530

    Added a notifier variable that will notify the authscreen about visibility of password

--- lib/Providers/AuthOptions.dart
+++ lib/Providers/AuthOptions.dart
@@ -2,11 +2,19 @@ import 'package:flutter/material.dart';
 
 class AuthOptions extends ChangeNotifier{
   bool loginState = true;
+  bool viewPass = false;
+  bool get fetchObscureTextInfo{
+    return viewPass;
+  }
   bool get fetchLoginState{
     return loginState;
   }
-  void change(){
+  void toggle(){
     loginState = !loginState;
+    notifyListeners();
+  }
+  void viewPassword(){
+    viewPass = !viewPass;
     notifyListeners();
   }
 }
