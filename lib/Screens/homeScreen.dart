e52a58773dfd43bbff7bfb28198d61caf8993329
Alpha17-2 <alphaisgod1@gmail.com>
2021-10-15 00:00:10 +0530

    logic divided for screens

--- lib/Screens/homeScreen.dart
+++ lib/Screens/homeScreen.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
-
 import 'package:firstapp/Helpers/deviceSize.dart';
+import 'package:firstapp/Screens/exploreScreen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
@@ -30,89 +30,7 @@ class homeScreen extends StatelessWidget {
                 ),
                 height: displayHeight(context) * 0.88,
                 width: displayWidth(context),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                        height: displayHeight(context) * 0.45,
-                        width: displayWidth(context),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(20),
-                              topRight: Radius.circular(20)),
-                          image: DecorationImage(
-                              image: AssetImage(
-                                  'images/08809940d40530bfdb457d7a60466657.jpg'),
-                              fit: BoxFit.cover),
-                        ),
-                        child: BackdropFilter(
-                          filter: ImageFilter.blur(sigmaX: 0.7, sigmaY: 0.7),
-                          child: Container(
-                            color: Colors.black.withOpacity(0.4),
-                            height: displayHeight(context) * 0.45,
-                            width: displayWidth(context),
-                            child: Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 10.0, right: 10, top: 12, bottom: 12),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      CircleAvatar(
-                                        radius: displayWidth(context) * 0.06,
-                                        backgroundColor: Colors.grey[300],
-                                        child: Center(
-                                          child: IconButton(
-                                              onPressed: () {},
-                                              icon: Icon(Icons.settings),
-                                              color: Colors.black,
-                                              iconSize: displayWidth(context) *
-                                                  0.055),
-                                        ),
-                                      ),
-                                      CircleAvatar(
-                                        radius: displayWidth(context) * 0.06,
-                                        backgroundColor: Colors.grey[300],
-                                        backgroundImage: AssetImage(
-                                            'images/friendsIcon2.png'),
-                                      )
-                                    ],
-                                  ),
-                                  Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    crossAxisAlignment: CrossAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        'Welcome,',
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(color: Colors.white,
-                                        fontWeight: FontWeight.w400,
-                                        fontSize: displayWidth(context)*0.05,
-                                        ),
-                                      ),
-                                      Text(
-                                        'Subhojeet Sahoo',
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(color: Colors.white,
-                                          fontSize: displayWidth(context)*0.07,
-                                          fontWeight: FontWeight.bold
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  Divider(),
-                                  Divider(),
-                                ],
-                              ),
-                            ),
-                          ),
-                        )),
-
-                  ],
-                ),
+                child: exploreScreen()
               )),
           Positioned(
             bottom: 0,
