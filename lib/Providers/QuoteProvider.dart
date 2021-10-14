6256bb9a1fa996b60d91197072f380895de8f279
Alpha17-2 <alphaisgod1@gmail.com>
2021-10-14 18:48:50 +0530

    added some quotes

--- lib/Providers/QuoteProvider.dart
+++ lib/Providers/QuoteProvider.dart
@@ -6,6 +6,30 @@ class QuoteProvider{
       quote: 'A friend knows the song in my heart and sings it to me when my memory fails.',
       author: 'Donna Roberts',
     ),
+    quoteModel(
+      author: 'Helen Keller',
+      quote: 'I would rather walk with a friend in the dark, than alone in the light.'
+    ),
+    quoteModel(
+      author: 'Donna Roberts',
+      quote: 'A friend knows the song in my heart and sings it to me when my memory fails.'
+    ),
+    quoteModel(
+      quote: 'What draws people to be friends is that they see the same truth. They share it.',
+      author: 'C.S Lewis',
+    ),
+    quoteModel(
+      author: 'Nicole Richie',
+      quote: 'True friends are like diamonds—bright, beautiful, valuable, and always in style.',
+    ),
+    quoteModel(
+      author: 'Heidi Wills',
+      quote: 'A friend is someone who makes it easy to believe in yourself.',
+    ),
+    quoteModel(
+      author: 'Jean de La Fontaine',
+      quote: 'Rare as is true love, true friendship is rarer.',
+    ),
   ];
   List<quoteModel> get fetchQuotesList{
     return [...quotes];
