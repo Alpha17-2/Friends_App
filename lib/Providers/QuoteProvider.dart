9524630baba30ceec7bc425f7f4dd8b1cda22fb2
subhodynamics <subhadeeppathak2013@gmail.com>
2021-10-15 10:36:56 +0530

    Added Friendship quotes

--- lib/Providers/QuoteProvider.dart
+++ lib/Providers/QuoteProvider.dart
@@ -30,6 +30,38 @@ class QuoteProvider{
       author: 'Jean de La Fontaine',
       quote: 'Rare as is true love, true friendship is rarer.',
     ),
+    quoteModel(
+      author: 'Elbert Hubbard',
+      quote: 'A friend is someone who knows all about you and still loves you.',
+    ),
+    quoteModel(
+      author: 'Alfred Tennyson',
+      quote: 'If I had a flower for every time I thought of you...I could walk through my garden forever.',
+    ),
+    quoteModel(
+      author: 'Aristotle',
+      quote: 'What is a friend? A single soul dwelling in two bodies.',
+    ),
+    quoteModel(
+      author: 'Socrates',
+      quote: 'Be slow to fall into friendship; but when thou art in, continue firm & constant.',
+    ),
+    quoteModel(
+      author: 'William Shakespeare',
+      quote: 'A friend is one that knows you as you are, understands where you have been, accepts what you have become, and still, gently allows you to grow.',
+    ),
+    quoteModel(
+      author: 'Plutarch',
+      quote: 'I don’t need a friend who changes when I change and who nods when I nod; my shadow does that much better.',
+    ),
+    quoteModel(
+      author: 'Friedrich Nietzsche',
+      quote: 'Love is blind; friendship closes its eyes.',
+    ),
+    quoteModel(
+      author: 'Mark Twain',
+      quote: 'Good friends, good books, and a sleepy conscience: this is the ideal life.',
+    ),
   ];
   List<quoteModel> get fetchQuotesList{
     return [...quotes];
