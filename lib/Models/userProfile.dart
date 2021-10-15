b316fc15c3c947efde0ffbd11e3aa44364a8c59e
Alpha17-2 <alphaisgod1@gmail.com>
2021-10-15 10:13:57 +0530

    userProfile class model

---
+++ lib/Models/userProfile.dart
@@ -0,0 +1,8 @@
+class userProfile {
+  final String title;
+  final String id;
+  final String email;
+  final String dp;
+  final String education;
+  userProfile({this.dp, this.education, this.email, this.id, this.title});
+}
