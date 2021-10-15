f24573a80849f8dd347aff041e213759c9d57001
Alpha17-2 <alphaisgod1@gmail.com>
2021-10-15 08:00:20 +0530

    screens added

---
+++ lib/Screens/myFriendsScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class myFriendsScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+
+      ],
+    );
+  }
+}
---
+++ lib/Screens/quotesScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class quotesScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+
+      ],
+    );
+  }
+}
