664df655989f5dcf33559f6528fb972a684ce0f9
Alpha17-2 <alphaisgod1@gmail.com>
2021-10-16 11:10:26 +0530

    api url changed

--- lib/Helpers/constants.dart
+++ lib/Helpers/constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class constants {
   String api =
-      'https://friends-app-241b7-default-rtdb.asia-southeast1.firebasedatabase.app/Profiles/';
+      'https://friends-app-241b7-default-rtdb.asia-southeast1.firebasedatabase.app/';
   String get fetchApi {
     return api;
   }
