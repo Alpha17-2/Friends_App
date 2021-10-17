cf73d28d8cd0651987d7998a37f1e73cce3ec3c8
Alpha17-2 <alphaisgod1@gmail.com>
2021-10-18 00:35:05 +0530

    mobile number field set

--- lib/Screens/addFriendScreen.dart
+++ lib/Screens/addFriendScreen.dart
@@ -83,187 +83,263 @@ class _addFriendScreenState extends State<addFriendScreen> {
       padding: const EdgeInsets.all(12.0),
       child: Form(
         key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Text(
-                "New Friend ",
+        child: SingleChildScrollView(
+          physics: NeverScrollableScrollPhysics(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Text(
+                  "New Friend ",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: displayWidth(context) * 0.06,
+                  ),
+                ),
+              ),
+              Opacity(
+                  opacity: 0,
+                  child: Divider(
+                    height: displayHeight(context) * 0.03,
+                  )),
+              Center(
+                  child: GestureDetector(
+                      onTap: () => pickImage,
+                      child: (_imageFile != null) ? dpExists() : dpNotExist())),
+              Opacity(
+                  opacity: 0,
+                  child: Divider(
+                    height: displayHeight(context) * 0.03,
+                  )),
+              Text(
+                'Basic Detail',
                 style: TextStyle(
                   color: Colors.black,
                   fontWeight: FontWeight.bold,
-                  fontSize: displayWidth(context) * 0.06,
+                  fontSize: displayWidth(context) * 0.045,
                 ),
               ),
-            ),
-            Opacity(
-                opacity: 0,
-                child: Divider(
-                  height: displayHeight(context) * 0.03,
-                )),
-            Center(
-                child: GestureDetector(
-                    onTap: () => pickImage,
-                    child: (_imageFile != null) ? dpExists() : dpNotExist())),
-            Opacity(
-                opacity: 0,
-                child: Divider(
-                  height: displayHeight(context) * 0.03,
-                )),
-            Text(
-              'Basic Detail',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: displayWidth(context) * 0.045,
+              Opacity(
+                  opacity: 0,
+                  child: Divider(
+                    height: displayHeight(context) * 0.02,
+                  )),
+              Text(
+                'Full Name',
+                style: TextStyle(color: Colors.black),
               ),
-            ),
-            Opacity(
-                opacity: 0,
-                child: Divider(
-                  height: displayHeight(context) * 0.02,
-                )),
-            Text(
-              'Full Name',
-              style: TextStyle(color: Colors.black),
-            ),
-            Opacity(
-                opacity: 0,
-                child: Divider(
-                  height: displayHeight(context) * 0.01,
-                )),
-            Container(
-              height: displayHeight(context) * 0.06,
-              width: displayWidth(context),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  border: Border.all(color: Colors.grey,width: 1.1)),
-              child: Padding(
-                padding: const EdgeInsets.only(top:10.0,left: 8.0,right: 8.0,bottom: 4.0),
-                child: Center(
-                  child: TextFormField(
-                    validator: (value) {
-                      if(value.isEmpty || value.length==0) return 'Cannot be empty';
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'Friend\'s name',
-                      border: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      enabledBorder: InputBorder.none,
-                      errorBorder: InputBorder.none,
-                      disabledBorder: InputBorder.none,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Opacity(
-                opacity: 0,
-                child: Divider(
-                  height: displayHeight(context) * 0.02,
-                )),
-            Text(
-              'Date of birth',
-              style: TextStyle(color: Colors.black),
-            ),
-            Opacity(
-                opacity: 0,
-                child: Divider(
-                  height: displayHeight(context) * 0.01,
-                )),
-            Container(
-              height: displayHeight(context) * 0.06,
-              width: displayWidth(context),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  border: Border.all(color: Colors.grey,width: 1.1)),
-              child: Padding(
-                padding: const EdgeInsets.only(top:10.0,left: 8.0,right: 8.0,bottom: 4.0),
-                child: Center(
-                  child: TextFormField(
-                    keyboardType: TextInputType.datetime,
-                    validator: (value) {
-                      if(value.isEmpty || value.length==0) return 'Cannot be empty';
-                      else{
-                        bool validDate = RegExp(r"^(0[1-9]|[12][0-9]|3[01])[- /.]").hasMatch(value);
-                        if(!validDate) return 'Please fill correct date';
-                        return null;
-                      }
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'dd/mm/yyyy',
-                      border: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      enabledBorder: InputBorder.none,
-                      errorBorder: InputBorder.none,
-                      disabledBorder: InputBorder.none,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Opacity(
-                opacity: 0,
-                child: Divider(
-                  height: displayHeight(context) * 0.02,
-                )),
-            Text(
-              'Gender',
-              style: TextStyle(color: Colors.black),
-            ),
-            Opacity(
-                opacity: 0,
-                child: Divider(
-                  height: displayHeight(context) * 0.01,
-                )),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  height: displayHeight(context)*0.06,
-                  width: displayWidth(context)*0.4,
-                  decoration: BoxDecoration(
+              Opacity(
+                  opacity: 0,
+                  child: Divider(
+                    height: displayHeight(context) * 0.01,
+                  )),
+              Container(
+                height: displayHeight(context) * 0.06,
+                width: displayWidth(context),
+                decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(5),
-                    border: Border.all(color: Colors.grey[300])
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Checkbox(value: isMale, onChanged:(value) {
-                        setState(() {
-                          if(!isMale) isMale = !isMale;
-                        });
-                      },),
-                      Text('Male',style: TextStyle(fontWeight: FontWeight.w700),),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: displayHeight(context)*0.06,
-                  width: displayWidth(context)*0.4,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      border: Border.all(color: Colors.grey[300])
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Checkbox(value: !isMale, onChanged:(value) {
-                        setState(() {
-                          if(isMale)
-                            isMale=!isMale;
-                        });
-                      },),
-                      Text('Female',style: TextStyle(fontWeight: FontWeight.w600),),
-                    ],
+                    border: Border.all(color: Colors.grey, width: 1.1)),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      top: 10.0, left: 8.0, right: 8.0, bottom: 4.0),
+                  child: Center(
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty || value.length == 0)
+                          return 'Cannot be empty';
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Friend\'s name',
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                      ),
+                    ),
                   ),
                 ),
-              ],
-            )
-          ],
+              ),
+              Opacity(
+                  opacity: 0,
+                  child: Divider(
+                    height: displayHeight(context) * 0.02,
+                  )),
+              Text(
+                'Date of birth',
+                style: TextStyle(color: Colors.black),
+              ),
+              Opacity(
+                  opacity: 0,
+                  child: Divider(
+                    height: displayHeight(context) * 0.01,
+                  )),
+              Container(
+                height: displayHeight(context) * 0.06,
+                width: displayWidth(context),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(color: Colors.grey, width: 1.1)),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      top: 10.0, left: 8.0, right: 8.0, bottom: 4.0),
+                  child: Center(
+                    child: TextFormField(
+                      keyboardType: TextInputType.datetime,
+                      validator: (value) {
+                        if (value.isEmpty || value.length == 0)
+                          return 'Cannot be empty';
+                        else {
+                          bool validDate =
+                              RegExp(r"^(0[1-9]|[12][0-9]|3[01])[- /.]")
+                                  .hasMatch(value);
+                          if (!validDate) return 'Please fill correct date';
+                          return null;
+                        }
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'dd/mm/yyyy',
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Opacity(
+                  opacity: 0,
+                  child: Divider(
+                    height: displayHeight(context) * 0.02,
+                  )),
+              Text(
+                'Gender',
+                style: TextStyle(color: Colors.black),
+              ),
+              Opacity(
+                  opacity: 0,
+                  child: Divider(
+                    height: displayHeight(context) * 0.01,
+                  )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    height: displayHeight(context) * 0.06,
+                    width: displayWidth(context) * 0.4,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        border: Border.all(color: Colors.grey[300])),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Checkbox(
+                          value: isMale,
+                          onChanged: (value) {
+                            setState(() {
+                              if (!isMale) isMale = !isMale;
+                            });
+                          },
+                        ),
+                        Text(
+                          'Male',
+                          style: TextStyle(fontWeight: FontWeight.w700),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: displayHeight(context) * 0.06,
+                    width: displayWidth(context) * 0.4,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        border: Border.all(color: Colors.grey[300])),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Checkbox(
+                          value: !isMale,
+                          onChanged: (value) {
+                            setState(() {
+                              if (isMale) isMale = !isMale;
+                            });
+                          },
+                        ),
+                        Text(
+                          'Female',
+                          style: TextStyle(fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Opacity(
+                  opacity: 0,
+                  child: Divider(
+                    height: displayHeight(context) * 0.03,
+                  )),
+              Text(
+                'Contact Detail',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: displayWidth(context) * 0.045,
+                ),
+              ),
+              Opacity(
+                  opacity: 0,
+                  child: Divider(
+                    height: displayHeight(context) * 0.02,
+                  )),
+              Text(
+                'Mobile Number',
+                style: TextStyle(color: Colors.black),
+              ),
+              Opacity(
+                  opacity: 0,
+                  child: Divider(
+                    height: displayHeight(context) * 0.01,
+                  )),
+              Container(
+                height: displayHeight(context) * 0.06,
+                width: displayWidth(context),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(color: Colors.grey, width: 1.1)),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      top: 10.0, left: 8.0, right: 8.0, bottom: 4.0),
+                  child: Center(
+                    child: TextFormField(
+                      keyboardType: TextInputType.phone,
+                      validator: (value) {
+                        if (value.isEmpty || value.length == 0)
+                          return 'Cannot be empty';
+                        else if (value.length != 10)
+                          return 'Mobile number should contain 10 digits';
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Enter 10 digit number',
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
