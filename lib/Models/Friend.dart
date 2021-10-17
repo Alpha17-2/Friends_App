d33afbd7a1455d900d0fcda334dce88a3027c007
Alpha17-2 <alphaisgod1@gmail.com>
2021-10-17 23:46:45 +0530

    modified model class for Friend

--- lib/Models/Friend.dart
+++ lib/Models/Friend.dart
@@ -6,7 +6,7 @@ class Friend {
   final String profession;
   final String interests;
   final String dp;
-  final DateTime dob;
+  final String dob;
   final String docId;
 
   Friend(this.title, this.gender, this.about, this.education, this.profession,
