7084fe282a48e41b14eb06067dc450945027d4fe
Alpha17-2 <alphaisgod1@gmail.com>
2021-10-17 19:27:51 +0530

    Model class for Friend

---
+++ lib/Models/Friend.dart
@@ -0,0 +1,13 @@
+class Friend {
+  final String title;
+  final String gender;
+  final String about;
+  final String education;
+  final String profession;
+  final String age;
+  final String interests;
+  final String dp;
+  final String docId;
+
+  Friend(this.title, this.gender, this.about, this.education, this.profession, this.age, this.interests, this.dp, this.docId);
+}
