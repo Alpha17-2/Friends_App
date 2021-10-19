7ffcbb7679c77a94120460ce15a5818bfb6f65d3
Alpha17-2 <alphaisgod1@gmail.com>
2021-10-19 23:07:58 +0530

    friend manager provider

--- lib/Providers/FriendsManager.dart
+++ lib/Providers/FriendsManager.dart
@@ -1,25 +1,27 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firstapp/Helpers/constants.dart';
 import 'package:firstapp/Models/Friend.dart';
+import 'package:firstapp/Models/friends.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class FriendsManager extends ChangeNotifier{
+class FriendsManager extends ChangeNotifier {
   // String denotes ID ,
-  Map<String,Friend> friendsMap = {};
+  Map<String, Friend> friendsMap = {};
 
   List<Friend> get list {
     return friendsMap.values.toList();
   }
 
-  Future<void> setFriends()async{
-    try{
-      Map<String,Friend> temp = {};
+  Future<void> setFriends() async {
+    try {
+      Map<String, Friend> temp = {};
       final String api = constants().fetchApi + 'friends.json';
       final response = await http.get(Uri.parse(api));
-      final data = json.decode(response.body) as Map<String,dynamic>;
+      final data = json.decode(response.body) as Map<String, dynamic>;
       data.forEach((key, value) {
         temp[key] = Friend(
           title: value['title'],
@@ -43,15 +45,55 @@ class FriendsManager extends ChangeNotifier{
       });
       friendsMap = temp;
       notifyListeners();
-    }
-    catch(error){
+    } catch (error) {
       print(error);
     }
   }
 
-  Future<void> addFriend(File image,Friend friend) async{
-
+  Future<void> addFriend(File image, Friend friend) async {
+    try {
+      final String api = constants().fetchApi + 'friends.json';
+      return http
+          .post(Uri.parse(api),
+              body: json.encode({
+                'title': friend.title,
+                'dob': friend.dob,
+                'education': friend.education,
+                'gender': friend.gender,
+                'about': friend.gender,
+                'profession': friend.profession,
+                'interests': friend.interests,
+                'instagram': friend.instagram,
+                'twitter': friend.twitter,
+                'youtube': friend.youtube,
+                'snapchat': friend.snapchat,
+                'facebook': friend.facebook,
+                'dp': '',
+                'contactNumber': friend.contactNumber,
+                'docId': '',
+                'mail': friend.mail,
+                'linkedin': friend.linkedin,
+              }))
+          .then((value) async {
+        final data = json.decode(value.body) as Map<String, dynamic>;
+        String docId = data['name'];
+        final api2 = constants().fetchApi + 'friends/${docId}.json';
+        String imageLocation = 'friends/${docId}/dp';
+        final Reference storageReference =
+        FirebaseStorage.instance.ref().child(imageLocation);
+        final UploadTask uploadTask = storageReference.putFile(image);
+        final TaskSnapshot taskSnapshot = await uploadTask;
+        taskSnapshot.ref.getDownloadURL().then((value) {
+          http.patch(Uri.parse(api2), body: json.encode(
+              {
+                'docId' : docId,
+                'dp' : value,
+              }
+          ));
+        });
+      });
+    } catch (error) {
+      print(error);
+    }
   }
-
-
 }
