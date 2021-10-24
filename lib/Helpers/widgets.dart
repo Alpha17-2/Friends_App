047c4359a84619a026be1c0aeca9afd8934696fb
Alpha17-2 <alphaisgod1@gmail.com>
2021-10-24 23:37:23 +0530

    Navigating to friend detail screen

--- lib/Helpers/widgets.dart
+++ lib/Helpers/widgets.dart
@@ -1,5 +1,6 @@
 import 'package:firstapp/Helpers/deviceSize.dart';
 import 'package:firstapp/Models/Friend.dart';
+import 'package:firstapp/Screens/friendDetailScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
 
@@ -8,89 +9,104 @@ Widget showMyFriends(BuildContext context, Friend f) {
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
     elevation: 12,
     child: Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CircleAvatar(
-              backgroundImage: NetworkImage(f.dp!),
-              radius: displayWidth(context) * 0.11,
-            ),
-            // Title and education
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
             Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                  
-                    f.title!,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: displayWidth(context) * 0.0355),
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => friendDetailScreen(
+                            friend: f,
+                          ),
+                        ));
+                  },
+                  child: Hero(
+                    tag: f.docId!,
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(f.dp!),
+                      radius: displayWidth(context) * 0.11,
+                    ),
                   ),
-                  Text(
-                    f.education!,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: Colors.black54,
-                        fontSize: displayWidth(context) * 0.031,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
-              )
-            ],
-          ),
-
-          // Best friend and close friend icons
-
-          Card(
-            elevation: 6,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(45)),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(45),
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [Colors.orange[400]!,Colors.orange[300]!,Colors.orange[200]!])
-          
-              ),
-              height: displayHeight(context)*0.125,
-              width: displayWidth(context)*0.075,
-              
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  IconButton(
-                    color: Colors.white,
-                    iconSize: displayWidth(context)*0.035,
-                    onPressed: () {
-                    
-                  }, icon: (Icon((f.isBestFriend!)?Ionicons.heart_circle:Ionicons.heart_circle_outline))),
-                  IconButton(
-                    color: Colors.white,
-                    iconSize: displayWidth(context)*0.035,
-                    onPressed: () {
-                    
-                  }, icon: (Icon((f.isBestFriend!)?Ionicons.star:Ionicons.star_outline))),
-                ],
-              ),
+                ),
+                // Title and education
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      f.title!,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: displayWidth(context) * 0.0355),
+                    ),
+                    Text(
+                      f.education!,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: displayWidth(context) * 0.031,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                )
+              ],
             ),
-          )
 
-        ],
-      )
-    ),
+            // Best friend and close friend icons
+
+            Card(
+              elevation: 6,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(45)),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(45),
+                    gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Colors.orange[400]!,
+                          Colors.orange[300]!,
+                          Colors.orange[200]!
+                        ])),
+                height: displayHeight(context) * 0.125,
+                width: displayWidth(context) * 0.075,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    IconButton(
+                        color: Colors.white,
+                        iconSize: displayWidth(context) * 0.035,
+                        onPressed: () {},
+                        icon: (Icon((f.isBestFriend!)
+                            ? Ionicons.heart_circle
+                            : Ionicons.heart_circle_outline))),
+                    IconButton(
+                        color: Colors.white,
+                        iconSize: displayWidth(context) * 0.035,
+                        onPressed: () {},
+                        icon: (Icon((f.isBestFriend!)
+                            ? Ionicons.star
+                            : Ionicons.star_outline))),
+                  ],
+                ),
+              ),
+            )
+          ],
+        )),
   );
 }
