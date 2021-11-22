ee1a02beaeeb29bc1d3f6d5183db251bdc9c4d3b
Alpha17-2 <alphaisgod1@gmail.com>
2021-11-22 11:09:30 +0530

    Implemented code to delete friend

--- lib/Helpers/widgets.dart
+++ lib/Helpers/widgets.dart
@@ -1,12 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firstapp/Helpers/deviceSize.dart';
 import 'package:firstapp/Models/Friend.dart';
+import 'package:firstapp/Providers/FriendsManager.dart';
 import 'package:firstapp/Screens/friendDetailScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
+import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 connectUsViaMail(String mail) async {
- final String url = 'mailto:$mail';
+  final String url = 'mailto:$mail';
   if (await canLaunch(url)) {
     await launch(url);
   } else {
@@ -19,7 +22,6 @@ Widget showMyFriends(BuildContext context, Friend f) {
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
     elevation: 12,
     child: Container(
-      
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(20),
@@ -44,41 +46,44 @@ Widget showMyFriends(BuildContext context, Friend f) {
                   },
                   child: Hero(
                     tag: f.docId!,
-                    child: (f.dp !=null && f.dp!='')?CircleAvatar(
-                      backgroundImage: NetworkImage(f.dp!),
-                      radius: displayWidth(context) * 0.09,
-                    ):CircleAvatar(
-                      backgroundImage: AssetImage((f.gender=='Male')?'images/male.jpg':'images/female.jpg'),
-                      radius: displayWidth(context) * 0.09,
-                    ),
+                    child: (f.dp != null && f.dp != '')
+                        ? CircleAvatar(
+                            backgroundImage: NetworkImage(f.dp!),
+                            radius: displayWidth(context) * 0.09,
+                          )
+                        : CircleAvatar(
+                            backgroundImage: AssetImage((f.gender == 'Male')
+                                ? 'images/male.jpg'
+                                : 'images/female.jpg'),
+                            radius: displayWidth(context) * 0.09,
+                          ),
                   ),
                 ),
                 // Title and education
                 Column(
                   mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Container(
-                      height: displayHeight(context)*0.03,
-                      width: displayWidth(context)*0.25,
+                      height: displayHeight(context) * 0.03,
+                      width: displayWidth(context) * 0.25,
                       child: Text(
                         f.title!,
                         //textAlign: TextAlign.center,
                         style: TextStyle(
-                          overflow: TextOverflow.ellipsis,
+                            overflow: TextOverflow.ellipsis,
                             color: Colors.black,
                             fontWeight: FontWeight.bold,
                             fontSize: displayWidth(context) * 0.0355),
                       ),
                     ),
                     Container(
-                      height: displayHeight(context)*0.03,
-                      width: displayWidth(context)*0.25,
+                      height: displayHeight(context) * 0.03,
+                      width: displayWidth(context) * 0.25,
                       child: Text(
                         f.education!,
-                      
                         style: TextStyle(
-                          overflow: TextOverflow.ellipsis,
+                            overflow: TextOverflow.ellipsis,
                             color: Colors.black54,
                             fontSize: displayWidth(context) * 0.031,
                             fontWeight: FontWeight.bold),
@@ -106,26 +111,43 @@ Widget showMyFriends(BuildContext context, Friend f) {
                           Colors.orange[300]!,
                           Colors.orange[200]!
                         ])),
-                height: displayHeight(context) * 0.15,
+                height: displayHeight(context) * 0.16,
                 width: displayWidth(context) * 0.075,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    IconButton(
-                        color: Colors.white,
-                        iconSize: displayWidth(context) * 0.035,
-                        onPressed: () {},
-                        icon: (Icon((f.isBestFriend!)
-                            ? Ionicons.heart_circle
-                            : Ionicons.heart_circle_outline))),
-                    IconButton(
-                        color: Colors.white,
-                        iconSize: displayWidth(context) * 0.035,
-                        onPressed: () {},
-                        icon: (Icon((f.isBestFriend!)
-                            ? Ionicons.star
-                            : Ionicons.star_outline))),
+                    Expanded(
+                      child: IconButton(
+                          color: Colors.white,
+                          iconSize: displayWidth(context) * 0.035,
+                          onPressed: () {},
+                          icon: (Icon((f.isBestFriend!)
+                              ? Ionicons.heart_circle
+                              : Ionicons.heart_circle_outline))),
+                    ),
+                    Expanded(
+                      child: IconButton(
+                          color: Colors.white,
+                          iconSize: displayWidth(context) * 0.035,
+                          onPressed: () {},
+                          icon: (Icon((f.isBestFriend!)
+                              ? Ionicons.star
+                              : Ionicons.star_outline))),
+                    ),
+                    Expanded(
+                      child: IconButton(
+                          color: Colors.white,
+                          iconSize: displayWidth(context) * 0.035,
+                          onPressed: () {
+                            Provider.of<FriendsManager>(context, listen: false)
+                                .deleteFriend(
+                                    FirebaseAuth.instance.currentUser!.uid
+                                        .toString(),
+                                    f.docId!);
+                          },
+                          icon: Icon(Icons.delete_forever)),
+                    )
                   ],
                 ),
               ),
